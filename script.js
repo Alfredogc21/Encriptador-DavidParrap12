58b8955d8cddac2be8a9453cf75b0c79ffc6fcc3
David Parra Pardo <98554197+DavidParrap12@users.noreply.github.com>
2024-02-18 20:04:50 -0500

    Add files via upload
    falta solucionar el margen del lado derecho

---
+++ script.js
@@ -0,0 +1,85 @@
+function validarTexto(texto) {
+
+    let caracteres = /[~!@#$%^&*()_+|}{[\]\\\/?><:"`;.,áéíóúàèìòù']/g;
+    let mayusculas = /[A-Z]/g;  
+    let vacio="";
+
+    if(texto.match(mayusculas)||texto.match(caracteres)){
+        alert("No se permiten caracteres especiales ni mayusculas");
+        return true;
+    }else if(texto==vacio){
+        alert("Ingrese un mensaje para encriptar");
+        return true;
+    }else {
+        return false;
+    }
+}
+    
+
+let btnencriptar = document.querySelector("#btn-encriptar");
+
+btnencriptar.addEventListener("click", function () {
+    let cajatexto = document.querySelector("#cajatexto").value;
+    let textoIngresado = cajatexto;
+
+    if(validarTexto (textoIngresado) == false) {
+        let Encriptado = encriptar(textoIngresado);
+        let resultado = document.querySelector("#msg");
+        resultado.value = Encriptado;
+    }else {
+        cajatexto = "";
+    }
+});
+
+const reglas = {"e":"enter","i":"imes","a":"ai","o":"ober","u":"ufat"};
+let imagen = document.querySelector("imagen2");
+let contenedor = document.querySelector("mensaje-encriptado");
+let contenedorResultado = document.querySelector("msg");
+
+function encriptar (textoIngresado) {
+    document.getElementById("imagen2").style.display = "none";
+    document.getElementById("mensaje-encriptado").style.display = "none";
+    document.getElementById("msg").style.display = "block";
+    document.getElementById("btn-copy").style.display = "block";
+
+    let Encriptado = textoIngresado;
+    for (const obj in reglas) {
+        Encriptado = Encriptado.replaceAll(obj,reglas[obj]);
+    }
+    return (Encriptado);
+}
+
+let btnCopiar = document.querySelector("#btn-copy");
+
+btnCopiar.addEventListener("click", function() {
+    let Copiado = document.querySelector("#msg").value;
+    navigator.clipboard.writeText(Copiado);
+    document.querySelector("#cajatexto").value;
+});
+
+let btnDesencriptar = document.querySelector("#btn-desencriptar");
+
+btnDesencriptar.addEventListener("click", function (){
+    let textoIngresado = document.querySelector("#cajatexto").value;
+    let Desencriptado = desencriptar(textoIngresado);
+    let resultado = document.querySelector("#msg");
+    resultado.value = Desencriptado;
+});
+
+
+function desencriptar(textoIngresado) {
+    document.getElementById("imagen2").style.display = "none";
+    document.getElementById("mensaje-encriptado").style.display = "none";
+    document.getElementById("msg").style.display = "block";
+    document.getElementById("btn-copy").style.display = "block";
+
+
+    let desencriptado = textoIngresado;
+    for (const obj in reglas) {
+        desencriptado = desencriptado.split(reglas[obj]).join(obj);
+    }
+    return desencriptado;
+}
+
+
+
